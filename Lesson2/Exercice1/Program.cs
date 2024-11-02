ae5160652fb9b91d3379875f808909a429361fe6
NikitaVolia <volanskijnikita2@gmail.com>
2024-11-02 18:43:03 +0100

    Lesson 2

---
+++ Lesson2/Exercice1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//створіть додаток, який відображає кількість парних,
+//непарних, унікальних елементів масиву.
+
+namespace Exercice1
+{
+
+
+    internal class Program
+    {
+        static int coupleCount(int[] nums)
+        {
+            Dictionary<int, int> numsDict = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (numsDict.ContainsKey(num))
+                    numsDict[num]++;
+                else
+                    numsDict[num] = 1;
+            }
+
+            int count = 0;
+            foreach (KeyValuePair<int, int> entry in numsDict)
+            {
+                if (entry.Value == 2)
+                    count++;
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 1, 4, 3, 2, 5, 4, 4 };
+            Console.WriteLine(coupleCount(arr));
+            Console.ReadLine();
+        }
+
+        
+    }
+}
