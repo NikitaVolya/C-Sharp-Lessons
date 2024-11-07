40438a118e2e6a1e393f49909697f7c85ed94c98
NikitaVoliqLP <volanskijnikita2@gmail.com>
2024-11-07 15:25:44 +0100

    end of lesson 4

--- Lesson4/Exercice3/Program.cs
+++ Lesson4/Exercice3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,9 @@ namespace Exercice3
     class CreditCard
     {
         UInt64 number;
+        UInt16 cvc;
+        UInt32 data;
+
         
         public string Number
         {
@@ -34,14 +38,97 @@ namespace Exercice3
                 number = Convert.ToUInt64(value);
             }
         }
+        public string OwnerFullName { get; set; }
+        public int CVC
+        {
+            get { return cvc; }
+            set {
+                if (value > 999 || value < 0)
+                    throw new ArgumentException("Credit card: Invalid CVC format.");
+                cvc = (ushort)Convert.ToInt16(value);
+            }
+        }
+
+        public string Data
+        {
+            get {
+                UInt32 day = data >> 27;
+                UInt32 month = data >> 23 & 15;
+                UInt32 year = data & 0b1111111111111111;
+                return Convert.ToString((int)day) + "/" + Convert.ToString((int)month) + "/" + Convert.ToString((int)year);
+            }
+            set
+            {
+                if (value.Length != 10)
+                    throw new ArgumentException("Credit card: Invalid date format.");
+                if (value[2] != '/' || value[5] != '/')
+                    throw new ArgumentException("Credit card: Invalid date format.");
+                UInt32 day = Convert.ToUInt32(value.Substring(0, 2));
+                
+                UInt32 month = Convert.ToUInt32(value.Substring(3, 2));
+                UInt32 year = Convert.ToUInt32(value.Substring(6));
+                if (day > 31 || month > 12)
+                    throw new ArgumentException("Credit card: Invalid date format.");
+
+                data = day << 27 | month << 23 | year;
+            }
+        }
+
+        public CreditCard(string pNumber, string pOwnerFullName, int pCVC, string pData)
+        {
+            Number = pNumber;
+            OwnerFullName = pOwnerFullName;
+            CVC = pCVC;
+            Data = pData;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Number} {CVC} {Data} {OwnerFullName}");
+        }
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            CreditCard a = new CreditCard { Number="1234567891234567"};
-            Console.WriteLine(a.Number);
+            CreditCard card;
+            try
+            {
+                card = new CreditCard("1234567891234567", "Nikita Volianskyi", 203, "07/11/2023");
+                card.Print();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                card = new CreditCard("12345678912345679", "Nikita Volianskyi", 203, "07/11/2023");
+                card.Print();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                card = new CreditCard("1234567891234567", "Nikita Volianskyi", 2033, "07/11/2024");
+                card.Print();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                card = new CreditCard("1234567891234567", "Nikita Volianskyi", 203, "72-22-2023");
+                card.Print();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadKey();
         }
     }
---
+++ Lesson4/Exerice4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Завдання 4
+//Користувач вводить до рядка з клавіатури математич-
+//ний вираз. Наприклад, 3*2*1*4. Програма має підрахувати
+//результат введеного виразу. У рядку можуть бути лише
+//цілі числа і оператор*. Для обробки помилок введення
+//використовуйте механізм виключень
+
+namespace Exerice4
+{
+
+    internal class Program
+    {
+        static int evalMultiple(string line)
+        {
+            int rep = 1;
+
+            int tmp = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '*')
+                {
+                    rep *= tmp;
+                    tmp = 0;
+                }
+                else if (Char.IsDigit(line[i]))
+                {
+                    tmp = tmp * 10 + line[i] - '0';
+                }
+                else
+                {
+                    throw new ArgumentException("Invalide value in string: " + line[i]);
+                }
+            }
+            rep *= tmp;
+            return rep;
+        }
+        static void Main(string[] args)
+        {
+            try {
+                Console.WriteLine(evalMultiple("2*12*2"));
+            } catch (ArgumentException e) { 
+                Console.WriteLine(e.Message); 
+            }
+            try
+            {
+                Console.WriteLine(evalMultiple("2*12*2.4"));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
