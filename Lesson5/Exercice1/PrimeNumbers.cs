6891361b67a876e6b95248b7f0007d4a0188901e
NikitaVolia <volanskijnikita2@gmail.com>
2024-11-12 22:28:56 +0100

    startt of lesson 5

---
+++ Lesson5/Exercice1/PrimeNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Numbers;
+
+namespace PrimeNumbers
+{
+    
+    class PrimeNumber
+    {
+
+        static public bool IsPrimeNumber(int number)
+        {
+            if (number < 0) return false;
+
+            bool[] values = new bool[number];
+            for (int i = 0; i < values.Length; i++)
+                values[i] = true;
+
+            for (int i = 2; i < values.Length; i++)
+            {
+                if (values[i])
+                {
+                    if (number % i == 0)
+                    {
+                        Console.WriteLine(i);
+                        return false;
+                    }
+                    int j = i + i;
+                    while (j < values.Length)
+                    {
+                        values[j] = false;
+                        j += i;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    
+}
---
+++ Lesson5/Exercice1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Завдання 1
+//Створіть класи для створення парних чисел, непарних
+//чисел, простих чисел, чисел Фібоначчі. Використовуйте
+//механізми просторів імен.
+
+using EvenNumbers;
+using NotEvenNumbers;
+using PrimeNumbers;
+
+namespace Exercice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenNumbers.Number number1 = new EvenNumbers.Number(10);
+            NotEvenNumbers.Number number2 = new NotEvenNumbers.Number(10);
+            
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            Console.WriteLine(PrimeNumbers.PrimeNumber.IsPrimeNumber(9857));
+
+            Console.ReadLine();
+        }
+    }
+}
